e3042f5e9ef9805728e1875aba5b8a9ce82ec8cc
Donna Chin <donnchin@gmail.com>
2021-12-29 15:56:28 -0500

    created book.js

---
+++ books.js
@@ -0,0 +1,5 @@
+/*
+Ref: https://www.linkedin.com/learning/javascript-code-challenges/put-your-javascript-skills-to-the-test?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=0
+*/
+
+//Question 1:
